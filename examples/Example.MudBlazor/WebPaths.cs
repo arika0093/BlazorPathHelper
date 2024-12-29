2611ed02e4cfb9981abf3a394c1af8ad607cc518
Arika Ishinami <delete0093@gmail.com>
2024-12-30 01:34:00 +0900

    docs: add implementation examples for MudBlazor integration in README and update WebPaths.cs

--- examples/Example.MudBlazor/WebPaths.cs
+++ examples/Example.MudBlazor/WebPaths.cs
@@ -1,5 +1,5 @@
 ﻿using BlazorPathHelper;
-using MudBlazor;
+using static MudBlazor.Icons.Material.Filled;
 
 // ReSharper disable MemberCanBePrivate.Global
 // ReSharper disable ClassNeverInstantiated.Global
@@ -9,21 +9,21 @@ namespace Example.MudBlazor;
 [BlazorPath]
 public partial class WebPaths
 {
-    [Item("Home", Icon = Icons.Material.Filled.House)]
+    [Item("Home", Icon = House)]
     public const string Home = "/";
-    [Item("Sample1", Icon = Icons.Material.Filled.Filter1)]
+    [Item("Sample1", Icon = Filter1)]
     public const string Sample1 = "/sample1";
-    [Item("Sample1C1", Icon = Icons.Material.Filled.ExposurePlus1)]
+    [Item("Sample1C1", Icon = ExposurePlus1)]
     public const string Sample1C1 = $"{Sample1}/child1";
-    [Item("Sample1C2", Icon = Icons.Material.Filled.ExposurePlus2)]
+    [Item("Sample1C2", Icon = ExposurePlus2)]
     public const string Sample1C2 = $"{Sample1}/child2";
-    [Item("Sample1C2C1", Icon = Icons.Material.Filled.StarBorder)]
+    [Item("Sample1C2C1", Icon = StarBorder)]
     public const string Sample1C2C1 = $"{Sample1}/child2/child1";
-    [Item("Sample2", Icon = Icons.Material.Filled.Filter2)]
+    [Item("Sample2", Icon = Filter2)]
     public const string Sample2 = "/sample2";
-    [Item("Sample2C1", Icon = Icons.Material.Filled._1xMobiledata)]
+    [Item("Sample2C1", Icon = _1xMobiledata)]
     public const string Sample2C1 = $"{Sample2}/child1";
-    [Item("Sample3", Icon = Icons.Material.Filled.Filter3)]
+    [Item("Sample3", Icon = Filter3)]
     public const string Sample3 = "/sample3";
 
     public const string Sample3Arg = "/sample3/{value:int}" ; 
