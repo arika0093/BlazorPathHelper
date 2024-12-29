4c45f140b233b4ebc7a7483d7f5d1dac4cfc254d
arika <Delete0093@Gmail.com>
2024-12-30 01:38:01 +0900

    docs: add MudBlazor example project (#23)
    * feat: Improved handling of icon definitions defined in svg format (e.g. MudBlazor)
    * docs: add mudblazor sample codes
    * docs: add implementation examples for MudBlazor integration in README and update WebPaths.cs

---
+++ examples/Example.MudBlazor/Program.cs
@@ -0,0 +1,27 @@
+using MudBlazor.Services;
+using Example.MudBlazor.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
