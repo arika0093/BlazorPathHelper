700b8b4079be5c7df09b5353348d79877514d900
Arika Ishinami <delete0093@gmail.com>
2025-04-10 01:18:34 +0900

    add sample codes

---
+++ examples/Example.ServerPathBase/Program.cs
@@ -0,0 +1,27 @@
+using Example.ServerPathBase;
+using Example.ServerPathBase.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// pathbase specified
+app.UsePathBase(WebPaths.PathBase);
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ examples/Example.ServerPathBase/WebPaths.cs
@@ -0,0 +1,37 @@
+﻿using BlazorPathHelper;
+using Example.ServerPathBase.Components.Pages;
+
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable ClassNeverInstantiated.Global
+
+namespace Example.ServerPathBase;
+
+[BlazorPath(PathBaseValue = PathBase)]
+public partial class WebPaths
+{
+    [Item(Ignore = true)]
+    internal const string PathBase = "/example/";
+
+    // Native Blazor Template does not include icons.
+    // In this example, Bootstrap Icons are added to App.razor as follows:
+    // <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    // ---------------------------------
+    [Item("Home", Icon = "bi-house-door-fill")]
+    [Page<Home>]
+    public const string Home = "/";
+
+    [Item("Sample1", Icon = "bi-1-circle-fill")]
+    [Page<Sample1>]
+    public const string Sample1 = "/sample1";
+
+    [Item("Sample2", Icon = "bi-2-circle-fill")]
+    [Page<Sample2>]
+    public const string Sample2 = "/sample2";
+
+    [Item("Sample3", Icon = "bi-3-circle-fill")]
+    [Page<Sample3>]
+    public const string Sample3 = "/sample3";
+
+    [Page<Sample3Value>]
+    public const string Sample3Arg = "/sample3/{value:int}" ; 
+}
